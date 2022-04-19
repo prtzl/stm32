82aded9f6e884f35e0addaa62ab9514be469862a
prtzl <matej.blagsic@protonmail.com>
2022-04-19 15:07:48 +0200

    Add SWO driver (C compatible)

---
+++ Project/SWO.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void SWO_PrintChar(char const c, uint8_t const portNumber);
+    void SWO_PrintString(char const* s, uint8_t const portNumber);
+    void SWO_PrintDefault(char const* str);
+    void SWO_PrintDefaultN(char const* str, size_t const len);
+
+#ifdef __cplusplus
+}
+#endif
