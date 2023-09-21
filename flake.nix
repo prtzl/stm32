84342e3bc98e768727ee623f4d119742caa81f0e
prtzl <matej.blagsic@protonmail.com>
2023-09-21 16:26:01 +0200

    Update flake lock, update flake direnv, so that clangd works (Add clang to env)

--- flake.nix
+++ flake.nix
@@ -50,9 +50,8 @@
       defaultApp = flash-jlink;
 
       devShell = pkgs.mkShellNoCC {
-        nativeBuildInputs = (firmware.nativeBuildInputs or [ ])
-          ++ [ pkgs.clang-tools jlink pkgs.stlink pkgs.dos2unix pkgs.glibc_multi ];
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_11.llvm ];
+        nativeBuildInputs = (firmware.nativeBuildInputs or [ ]) ++ (firmware.buildInputs or [ ])
+          ++ [ pkgs.clang-tools jlink pkgs.stlink pkgs.dos2unix pkgs.glibc_multi pkgs.clang ];
       };
     });
 }
